422b6cc470754cd73c4c1833b92218ee9dd02fc8
Vishusharma001 <vishusharma6425@gmail.com>
2024-07-12 12:56:41 +0530

    new git repo

---
+++ src/BasicsOfJava/basics.java
@@ -0,0 +1,11 @@
+package BasicsOfJava;
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("BCA");
+        System.out.println("tcp");
+        System.out.println("ip");
+        System.out.println("Application layer");
+    }
+}
